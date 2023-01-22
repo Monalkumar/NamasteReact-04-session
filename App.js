d7ada690b43bf38b0d9dbc06d1d2e8a74194e597
MonalKumar <monalshiv123@gmail.com>
2023-01-23 01:43:41 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement("h1", {id:"title"} ,"hello world");
+const heading1 = React.createElement("h2", {id:"hello"} ,"hello world");
+const container = React.createElement("div", {id:"container"} ,[heading, heading1]);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(container)
+
+import React from "react";
