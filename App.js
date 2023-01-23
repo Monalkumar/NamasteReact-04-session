03e67e183ab2dbbd3b2f6fa5aad278b4debbac3f
MonalKumar <monalshiv123@gmail.com>
2023-01-24 00:38:28 +0530

    coding-react

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1", {id:"title"} ,"hello world");
 const heading1 = React.createElement("h2", {id:"hello"} ,"hello world");
-const container = React.createElement("div", {id:"container"} ,[heading, heading1]);
+const heading2 = React.createElement("h2", {id:"hello"} ,"hello world coding");
+const container = React.createElement("div", {id:"container"} ,[heading, heading1,heading2]);
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(container)
 
