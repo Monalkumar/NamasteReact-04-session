adfbf3c1210334f412ba84f67efd840841826b2c
MonalKumar <monalshiv123@gmail.com>
2023-01-24 12:22:11 +0530

    hi

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1", {id:"title"} ,"hello world");
-const heading1 = React.createElement("h2", {id:"hello"} ,"hello world");
-const heading3 = React.createElement("h2", {id:"hello"} ,"hello world");
-const heading4 = React.createElement("h2", {id:"hello"} ,"hello world");
-const heading5 = React.createElement("h2", {id:"hello"} ,"hello world");
+const heading1 = React.createElement("h2", {id:"hello"} ,"Pizza");
+const heading3 = React.createElement("h2", {id:"hello"} ,"Burger");
+const heading4 = React.createElement("h2", {id:"hello"} ,"sandwich");
+const heading5 = React.createElement("h2", {id:"hello"} ,"cocktail");
 const heading2 = React.createElement("h2", {id:"hello"} ,"hello world coding");
 const container = React.createElement("div", {id:"container"} ,[heading, heading1,heading2,heading3,heading4,heading5]);
 const root = ReactDOM.createRoot(document.getElementById("root"))
